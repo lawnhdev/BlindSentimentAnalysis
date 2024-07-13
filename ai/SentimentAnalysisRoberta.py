b66249b167e16007a7c69b4ad891b0e482f2b76e
michael-deprospo <deprosme@dukes.jmu.edu>
2024-07-13 18:32:56 -0400

    "gleep"

--- ai/SentimentAnalysisRoberta.py
+++ ai/SentimentAnalysisRoberta.py
@@ -7,8 +7,15 @@ import pprint
 from scipy.special import softmax
 from Cleaner import clean_post_data
 from transformers import logging
+import ImportCSVAll 
+import argparse
+import ImportCSV
+import sqlite3
+import datetime
 logging.set_verbosity_error()
+
 dataset_path = "../data/Post.csv"
+
 MODEL = f"cardiffnlp/twitter-roberta-base-sentiment-latest"
 
 # Analyzes a specific text body
@@ -55,11 +62,34 @@ def analyze_dataset(path):
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser("Generate sentiment for a company")
+    parser.add_argument("company", nargs='?', help="Company to generate for all history", type=str)
+    args = parser.parse_args()
+    if args.company == None:
+        ImportCSVAll.write_to_csv()
+    else:
+        ImportCSV.write_to_csv_single(args.company)
     df = analyze_dataset(dataset_path)
     # group by company and get the average sentiment scores 
     #TODO use a weighted average when we have likes, comments, and views
     averages_df = df.groupby('company')[['neutral', 'positive', 'negative']].mean().reset_index()
+    now = datetime.datetime.now()
+    averages_df['date_computed'] = str(now)
     # replace with writing to the db
+    conn = sqlite3.connect('../blind_posts.db')
+    # cursor = conn.cursor()
+    # cursor.execute('''CREATE TABLE IF NOT EXISTS Sentiment (
+    #             Comment_ID INTEGER PRIMARY KEY,
+    #             Company TEXT,
+    #             Neutral REAL,
+    #             Positive REAL,
+    #             Negative REAL,
+    #             Date_Published TEXT,
+    #         )''')
+    averages_df.to_sql('sentiment_scores', conn, if_exists='append')
+    # cursor.execute('''INSERT INTO Sentiment (Company, Neutral, Positve, Negative, Date_Published )
+    #             VALUES (?, ?, ?, ?, ?, ?, ?)''', averages_df)
+    
     pprint.pp(averages_df)
         
 
