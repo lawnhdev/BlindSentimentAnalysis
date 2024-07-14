42a754d1a88e88d476983ce85161b0c66da15111
michael-deprospo <deprosme@dukes.jmu.edu>
2024-07-13 22:19:31 -0400

    skleee

--- ai/Fudge.py
+++ ai/Fudge.py
@@ -1,6 +1,7 @@
 import sqlite3
 import random
 from datetime import datetime, timedelta
+import numpy as np
 companies = ["Meta","Google","Cisco","Oracle","Airbnb"]
 conn = sqlite3.connect('../blind_posts.db')
 c = conn.cursor()
@@ -9,20 +10,21 @@ c.execute('''CREATE TABLE "sentiment_scores" (
     "neutral" REAL,
     "positive" REAL,
     "negative" REAL,
-    "date_computed" TEXT,
+    "date_computed" TIMESTAMP,
     "type" INTEGER
     )''')
 for i in range(0,5):
     rand = random.random()
     for k in range(0,100):
+        random_numbers = np.random.dirichlet([1, 1, 1])
         rand2 = random.random()
         sentiment = (
             # k,
             companies[i],
-            rand * rand2,
-            1 - (rand * rand2),
-            (1 - rand) * rand2,
-            str(datetime.now() + timedelta(days=-k)), 
+            random_numbers[0],
+            random_numbers[1],
+            random_numbers[2],
+            datetime.now() + timedelta(days=-k), 
             # left as 0 for now, maybe %3 to get spread for later idk how to use rn
             0
         )
